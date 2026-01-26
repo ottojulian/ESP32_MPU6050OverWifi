37b181b4f2fe36f681528083668722f082539cf6
Jvvvvlian <43147225+ottojulian@users.noreply.github.com>
2026-01-26 13:51:02 -0300

    Refactor ESP32 code for accelerometer and WiFi setup
    Refactor pensado para los joysticks del juego de Química Computacional

--- codigo_esp32.ino
+++ codigo_esp32.ino
@@ -1,84 +1,188 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
-
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <OSCMessage.h>
-#include <OSCBundle.h>
 #include <WiFiUdp.h>
 #include <WiFi.h>
 
 
-////////////////////////////////
-///////////WIFI SETUP///////////
-////////////////////////////////
-// Replace with your network credentials
+// IMPORTANTE!!//
+// Compilar usando el board ES32 Dev Module, sinó o no compila, o compila pero no conecta al wifi.
+// Ahora si, el código:
 
-IPAddress staticIP(10, 1, 101, 171);     // Set your static IP address
-IPAddress gateway(10, 1, 103, 254);        // Gateway IP address (usually your router's IP)
-IPAddress subnet(255, 255, 252, 0);       // Subnet mask
+////////////////////////////////
+//////// INIT VARIABLES ////////
+////////////////////////////////
 
-// OSC server details
-const IPAddress outIp(10,1,101,30);     // Set your static IP address
-const unsigned int outPort = 9000;            // Port number of the OSC server
-const unsigned int localPort = 8888;
-//MACADDRESS = C8:F0:9E:53:07:34
+
+// Loop Rate:
+int mseg_delay = 10;
+
+// EWMA:
+int umbral = 20; //--> variable vieja, reivsar
+float alpha = 0.2;
+float wma_x = 0.0;
+float wma_y = 0.0;
+float wma_z = 0.0;
+float wma_rol = 0.0;
+float wma_pic = 0.0;
+float wma_yaw = 0.0;
+
+//OSC addresses:
+const char addr_sensores[] = "/4/sensores";
+const char addr_loopRate[] = "/4/loopRate";
+const char addr_ewmaAlpha[] = "/4/ewmaAlpha";
+
+////////////////////////////////
+//////// NETWORK SETUP /////////
+////////////////////////////////
+
+// IP:
+IPAddress staticIP(192, 168, 1, 102);  // IP local ### Me parece que no le da bola a esto
+IPAddress gateway(192, 168, 1, 1);    // Gateway
+IPAddress subnet(255, 255, 255, 0);   // Subnet mask
+
+const IPAddress outIp(192, 168, 1, 255);  // IP destino ### Probar con 255 para broadcast
+
+// UDP:
+const unsigned int outPort = 9000;    // Puerto UPD destino
+const unsigned int localPort = 8000;  // Puerto UPD destino
+
+// Autenticación WIFI:
+
+char ssid[] = "lowpoly99";
+char pass[] = "lowpoly99";
+
+/*
+char ssid[] = "open-score";
+char pass[] = "0p3n-5c0r3";
+
+
 char ssid[] = "LAB1507";
 char pass[] = "7051BAL!";
+*/
+////////////////////////////////
+////////// INSTANCIAS //////////
+////////////////////////////////
 
-
-WiFiUDP Udp;        // Create an instance of the UDP class
+WiFiUDP Udp;
 Adafruit_MPU6050 mpu;
 
 ////////////////////////////////
-////////PIEZOELECTRICO//////////
+////////// FUNCIONES ///////////
 ////////////////////////////////
 
-const int piezoIn = 34;
-int piezoValue = 0;
-bool piezoBool = false;
-int umbral = 0;
-
 float umbralToBool(int value, int umbral) {
- 
-  if (piezoValue >= umbral){
-    
+  /* Detecta si hubo golpe */
+  if (value >= umbral) {
+
     return 1.0;
   } else {
     return 0.0;
   }
 }
 
+void loopRate(OSCMessage &msg) {
+  /* Cambia los milisegundos del delay al final del loop */
+  if (msg.isInt(0)) {
+    if (msg.getInt(0) >= 5) {
+      mseg_delay = msg.getInt(0);
+      Serial.print("Nuevo Loop Rate: ");
+      Serial.println(mseg_delay);
+      Serial.print("");
+    }
+  }
+}
+
+void ewmaAlpha(OSCMessage &msg) {
+  /* Calibra el alpha para el suavizado de los valores de acc y gyr */
+  if (msg.isFloat(0)) {
+    umbral = msg.getFloat(0);
+    if (umbral >= 0 && umbral <= 1) {
+      Serial.print("Nuevo Alpha: ");
+      Serial.println(alpha);
+      Serial.print("");
+    } else {
+      Serial.print("Valor alpha fuera de rango! (");
+      Serial.print(umbral);
+      Serial.println(")");
+    }
+  }
+}
+
+void receiveMessage() {
+  /* Recive mensaje OSC */
+  OSCMessage inmsg;  // Crea mensaje para recibir valores
+  int size = Udp.parsePacket();
+  if (size > 0) {
+    while (size--) {
+      inmsg.fill(Udp.read());
+    }
+    if (!inmsg.hasError()) {
+      inmsg.dispatch(addr_loopRate, loopRate);
+      inmsg.dispatch(addr_ewmaAlpha, ewmaAlpha);
+    } else {
+      auto error = inmsg.getError();
+      Serial.print("ERROR en mensaje entrante: ");
+      Serial.println(error);
+      Serial.print("");
+    }
+  }
+}
+
+////////////////////////////////
+//////////// SETUP /////////////
+////////////////////////////////
+
 void setup(void) {
+  // Inicialización Serial:
   Serial.begin(115200);
 
-umbral = 20; // Umbral para el piezoelectrico
+  //Inicialización Serial (para sensores):
+  /*
+  while (!Serial) {
+    delay(10);                    // Espera a que conecte el serial
+  }
+  */
 
-//Inicialización WIFI
+  delay(1000);
+
+  Serial.println("////////////////////////////////");
+  Serial.println("////////// RAQUETA 2 ///////////");
+  Serial.println("////////////////////////////////");
+
+  //Inicialización WIFI:
+  Serial.println("-Conectando a Wi-Fi-");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect(true);
+  delay(100);
 
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print("status = ");
+    Serial.println(WiFi.status());
   }
-  Serial.println("Connected to Wi-Fi");
 
-  // Start UDP
-  Udp.begin(12345);  // Local port to listen on (can be any unused port)
+  Serial.print("Conectado a Wi-Fi");
 
+  // Inicialización UDP:
+  Udp.begin(localPort);
 
-////////////////////////////////
-//////////// MPU6050 ///////////
-////////////////////////////////
+  Serial.print("UDP iniciado - escuchando en: ");
+  Serial.print(WiFi.localIP());
+  Serial.print(":");
+  Serial.println(localPort);
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
 
-  //Inicialización Serial (para sensores)
+  ////////////////////////////////
+  //////////// MPU6050 ///////////
+  ////////////////////////////////
 
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-
+  // Inicialización de MPU6050:
   Serial.println("Adafruit MPU6050 test!");
-
-  // Try to initialize!
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -87,94 +191,108 @@ umbral = 20; // Umbral para el piezoelectrico
   }
   Serial.println("MPU6050 Found!");
 
+  // Setup del MPU6050: ### Hay que revisar esto
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   Serial.print("Accelerometer range set to: ");
   switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
   }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   Serial.print("Gyro range set to: ");
   switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
   }
-
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   Serial.print("Filter bandwidth set to: ");
   switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
   }
 
   Serial.println("");
   delay(100);
 }
 
+////////////////////////////////
+//////////// LOOP //////////////
+////////////////////////////////
 
 void loop() {
+  // Recibir mensajes OSC por UPD:
+  receiveMessage();
 
-  //oscMsg.empty();  // Clear previous message
-  //oscMsg.setAddress("/test/address");  // Set the OSC address 
-  OSCMessage oscMsg("/test/address");  // Create an instance of the OSCMessage class
+  // Crear el mensaje OSC a enviar:
+  OSCMessage oscMsg(addr_sensores);
 
-  //oscMsg.add(3.14f);                // Add a float argument
-  
-
-  // Send the OSC message
-  
-  piezoValue = analogRead(piezoIn);
-  //Serial.println(piezoValue);
-  piezoBool = umbralToBool (piezoValue, umbral);
-
-  /* Get new sensor events with the readings */
+  //// Acelerómetro y Giroscopio ////
+  // Tomar los eventos de los sensores:
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  oscMsg.add(a.acceleration.x).add(a.acceleration.y).add(a.acceleration.z).add(g.gyro.x).add(g.gyro.y).add(g.gyro.z).add(0.0f + piezoBool);                    // Add an integer argument
-  
+
+  // EWMA - calcular promedios:
+  wma_x = alpha * a.acceleration.x + (1 - alpha) * wma_x;
+  wma_y = alpha * a.acceleration.y + (1 - alpha) * wma_y;
+  wma_z = alpha * a.acceleration.z + (1 - alpha) * wma_z;
+  wma_rol = alpha * g.gyro.x + (1 - alpha) * wma_rol;
+  wma_pic = alpha * g.gyro.y + (1 - alpha) * wma_pic;
+  wma_yaw = alpha * g.gyro.z + (1 - alpha) * wma_yaw;
+
+  //// Envío de mensaje OSC ////
+  // Agregar valores al mensaje:
+  //oscMsg.add(a.acceleration.x).add(a.acceleration.y).add(a.acceleration.z).add(g.gyro.x).add(g.gyro.y).add(g.gyro.z);   // versión sin smoothing
+  oscMsg.add(wma_x).add(wma_y).add(wma_z).add(wma_rol).add(wma_pic).add(wma_yaw);
+
+  // Enviar mensaje OSC por UDP:
   Udp.beginPacket(outIp, outPort);
   oscMsg.send(Udp);
   Udp.endPacket();
-/*
+  oscMsg.empty();  // Vacía el mensaje para recibir los próximos valores
+
+  ////////////////////////////////
+  //////// NETWORK SETUP /////////
+  ////////////////////////////////
+  /*
   // Print out the values
   Serial.print("(x, y, z): ");
   Serial.print(a.acceleration.x);
@@ -192,12 +310,11 @@ void loop() {
   Serial.print(g.gyro.z);
   Serial.print(". ");
 
-//  Serial.print("Temperature: ");
-//  Serial.print(temp.temperature);
-//  Serial.println(" degC");
-*/
-  Serial.println(piezoBool);
-  Serial.println("");
-  oscMsg.empty();
-  delay(5);
+  //  Serial.print("Temperature: ");
+  //  Serial.print(temp.temperature);
+  //  Serial.println(" degC");
+  */
+
+  //// Regulación del rate del script ////
+  delay(mseg_delay);
 }
